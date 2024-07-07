f02d652dddefac4a2e1f40327e315cae0a7f55d2
unknown <komangweda360@gmail.com>
2024-07-07 15:44:19 +0800

    Tampilan Login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,48 +1,100 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Login | Reservasi Hotel</title>
+    <link rel="icon" href="{{ asset('img/icon.svg') }}" type="image/icon type">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <title>Document</title>
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="{{ asset('lte/plugins/fontawesome-free/css/all.min.css') }}">
+    <!-- icheck bootstrap -->
+    <link rel="stylesheet" href="{{ asset('lte/plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="{{ asset('lte/dist/css/adminlte.min.css') }}">
+    <style>
+        .login-page {
+            background: url('{{ asset('img/background.jpg') }}') no-repeat center center fixed;
+            background-size: cover;
+        }
+    </style>
 </head>
-<body>
-
-@if(session('status'))
-        <div class="alert alert-success">
-            {{ session('status') }}
-        </div>
-@endif
-
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-6">
-            <div class="card card-primary">
-                <div class="card-header">
-                    <h3 class="card-title">Test Login</h3>
-                </div>
-                <form action="/login" method="POST">
-                    @csrf
-                    <div class="card-body">
-                        <div class="form-group">
-                            <label for="">Email</label>
-                            <input type="text" name="email" class="form-control" placeholder="Enter email">
-                        </div>
-                        <div class="form-group">
-                            <label for="">Password</label>
-                            <input type="text" name="password" class="form-control" placeholder="Enter password">
-                        </div>
-                    </div>
-                    <div class="card-footer">
-                        <button type="submit" class="btn btn-primary">Submit</button>
-                    </div>
-                    <div>Belum punya akun?<a href="/register">Register</a></div>
-                </form>
+<body class="hold-transition login-page">
+    <div class="login-box">
+    <!-- /.login-logo -->
+        <div class="card card-outline card-warning">
+            <div class="card-header text-center">
+                <a href="../../index2.html" class="h1">Reservasi Hotel</a>
             </div>
-        </div>
-    </div>
-</div>
+            <div class="card-body">
+            <p class="login-box-msg">Sign in to start your session</p>
 
+                <form action="../../index3.html" method="post">
+                    <label for="" class="text-muted">Username</label>
+                    <div class="input-group mb-3">
+                        <input type="text" class="form-control" placeholder="Enter username...">
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-envelope"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <label for=""  class="text-muted">Password</label>
+                    <div class="input-group mb-3">
+                        <input type="password" class="form-control" placeholder="Enter password...">
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-lock"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row mb-4">
+                        <div class="col-5">
+                            <!-- <div class="icheck-primary">
+                                <input type="checkbox" id="remember">
+                                <label for="remember">
+                                    Remember Me
+                                </label>
+                            </div> -->
+                        </div>
+                        <!-- /.col -->
+                        <div class="col-7">
+                            <button type="submit" class="btn btn-primary btn-block">Submit</button>
+                        </div>
+                        <!-- /.col -->
+                    </div>
+                </form>
+
+                <!-- <div class="social-auth-links text-center mt-2 mb-3">
+                    <a href="#" class="btn btn-block btn-primary">
+                        <i class="fab fa-facebook mr-2"></i> Sign in using Facebook
+                    </a>
+                    <a href="#" class="btn btn-block btn-danger">
+                        <i class="fab fa-google-plus mr-2"></i> Sign in using Google+
+                    </a>
+                </div> -->
+                <!-- /.social-auth-links -->
+
+                <p class="mb-1">
+                    <a href="forgot-password.html">I forgot my password</a>
+                </p>
+                <p class="mb-0">
+                    <a href="register.html" class="text-center">Register a new membership</a>
+                </p>
+            </div>
+            <!-- /.card-body -->
+        </div>
+        <!-- /.card -->
+    </div>
+    <!-- /.login-box -->
+
+    <!-- jQuery -->
+    <script src="{{ asset('lte/plugins/jquery/jquery.min.js') }}"></script>
+    <!-- Bootstrap 4 -->
+    <script src="{{ asset('lte/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <!-- AdminLTE App -->
+    <script src="{{ asset('lte/dist/js/adminlte.min.js') }}"></script>
 </body>
 </html>
